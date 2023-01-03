85a18381ca098644ccd3ecd6195bbaa553b43497
talisbreda <talis.breda@outlook.com>
2023-01-02 21:26:57 -0300

    Strapi landing pages projetct

--- react-teste/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-import "./styles/global-styles.css";
-
-import { Home } from "./templates/Home";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
